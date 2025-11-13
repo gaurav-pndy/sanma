1860c82b7f11396ecfefe3a691409c36e1294326
gaurav-pndy <gauravpndy00@gmail.com>
2025-11-13 21:12:05 +0530

    added specialities

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,6 @@ const navItems = [
   {
     name: "Products",
     href: "/products",
-    hasDropdown: true,
     submenu: [
       {
         id: 1,
@@ -34,7 +33,54 @@ const navItems = [
       },
     ],
   },
-  { name: "Specialities", href: "/specialities" },
+  {
+    name: "Specialities",
+    href: "/specialities",
+    submenu: [
+      {
+        id: 1,
+        name: "Nuero",
+        href: "/specialities/neuro",
+        normal: "/special/Normal/Neuro.svg",
+        selected: "/special/Selected/Neuro.svg",
+      },
+      {
+        id: 2,
+        name: "Spine",
+        href: "/specialities/spine",
+        normal: "/special/Normal/Spine.svg",
+        selected: "/special/Selected/Spine.svg",
+      },
+      {
+        id: 3,
+        name: "Opthalmology",
+        href: "/specialities/opthalmology",
+        normal: "/special/Normal/Opthal.svg",
+        selected: "/special/Selected/Opthal.svg",
+      },
+      {
+        id: 4,
+        name: "ENT",
+        href: "/specialities/ent",
+        normal: "/special/Normal/ENT.svg",
+        selected: "/special/Selected/ENT.svg",
+      },
+      {
+        id: 5,
+        name: "Plastic & Reconstructive",
+        href: "/specialities/plastic-and-reconstructive",
+        normal: "/special/Normal/P&R.svg",
+        selected: "/special/Selected/P&R.svg",
+      },
+      {
+        id: 6,
+        name: "Dental",
+        href: "/specialities/dental",
+        normal: "/special/Normal/Dental.svg",
+        selected: "/special/Selected/Dental.svg",
+      },
+    ],
+  },
   { name: "Contact", href: "/contact" },
 ];
 
@@ -65,6 +111,7 @@ export default function Header() {
   const [open, setOpen] = useState(false);
   const [openDropdown, setOpenDropdown] = useState(null);
   const [productsDropdown, setProductsDropdown] = useState(false);
+  const [specialDropdown, setSpecialDropdown] = useState(false);
   const pathname = usePathname();
 
   // Prevent body scroll when mobile menu is open
@@ -89,7 +136,7 @@ export default function Header() {
 
   // Desktop Dropdown Component
   const DesktopProductsDropdown = () => {
-    const productsItem = navItems.find((item) => item.hasDropdown);
+    const productsItem = navItems.find((item) => item.name == "Products");
 
     return (
       <div
@@ -116,14 +163,14 @@ export default function Header() {
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 1, y: 10 }}
               //   transition={{ duration: 0.2 }}
-              className="absolute left-1/2 translate-x-[-50%] mt-4 w-lg xl:w-2xl bg-[#f4eeeb] rounded-lg overflow-hidden shadow-2xl  transition-all duration-300  z-50"
+              className="absolute left-1/2 translate-x-[-50%] mt-4 w-lg xl:w-2xl bg-white/60 backdrop-blur-lg rounded-lg overflow-hidden shadow-2xl  transition-all duration-300  z-50"
             >
               <div className=" grid grid-cols-3 ">
                 {productsItem.submenu.map((product) => (
                   <Link
                     key={product.id}
                     href={product.href}
-                    className="flex flex-col items-center gap-6 p-8 pb-2  border-b-4 border-transparent hover:border-[#5493d7] hover:bg-[#f7f7f7] transition-all duration-300 group/item"
+                    className="flex flex-col items-center gap-6 p-8 pb-4 border-b-4 border-transparent hover:border-[#5493d7] hover:bg-[#f7f7f7] transition-all duration-300 group/item"
                   >
                     {/* Product Image */}
                     <div className="relative w-28 xl:w-40 h-20 xl:h-32 rounded-lg shrink-0 overflow-hidden">
@@ -151,6 +198,74 @@ export default function Header() {
       </div>
     );
   };
+  const DesktopSpecialDropdown = () => {
+    const specialsItem = navItems.find((item) => item.name == "Specialities");
+
+    return (
+      <div
+        onMouseEnter={() => setSpecialDropdown(true)}
+        onMouseLeave={() => setSpecialDropdown(false)}
+        className="relative group  py-4"
+      >
+        <button
+          className={`flex items-center gap-1 text-sm font-light transition-all duration-300 cursor-pointer ${
+            isActive("/specialities")
+              ? "text-white underline underline-offset-4 font-normal decoration-2"
+              : "text-white group-hover:underline underline-offset-4 decoration-2"
+          }`}
+        >
+          {specialsItem.name}
+          {/* <FaChevronDown className="w-4 h-4 transition-transform group-hover:rotate-180" /> */}
+        </button>
+
+        {/* Dropdown Menu */}
+        <AnimatePresence>
+          {specialDropdown && (
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 1, y: 10 }}
+              //   transition={{ duration: 0.2 }}
+              className="absolute left-1/2 translate-x-[-50%] mt-4 w-3xl xl:w-5xl bg-white/60 backdrop-blur-lg rounded-lg overflow-hidden shadow-2xl  transition-all duration-300  z-50"
+            >
+              <div className=" grid grid-cols-6 ">
+                {specialsItem.submenu.map((s) => (
+                  <Link
+                    key={s.id}
+                    href={s.href}
+                    className="flex flex-col items-center gap-6 p-8 pb-4 border-b-4 border-transparent hover:border-[#5493d7] hover:bg-[#f7f7f7] transition-all duration-300 group/item"
+                  >
+                    {/* Product Image */}
+                    <div className="relative w-16 h-16 rounded-lg shrink-0 overflow-hidden group/item">
+                      <Image
+                        src={s.normal}
+                        alt={s.name}
+                        fill
+                        className="object-cover transition-opacity duration-500 group-hover/item:opacity-0"
+                      />
+                      <Image
+                        src={s.selected}
+                        alt={s.name}
+                        fill
+                        className="object-cover opacity-0 transition-opacity duration-300 group-hover/item:opacity-100"
+                      />
+                    </div>
+
+                    {/* Product Info */}
+                    <div className="flex flex-col justify-center">
+                      <h4 className="text-xs xl:text-sm text-center font-semibold text-[#384F5D] ">
+                        {s.name}
+                      </h4>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    );
+  };
 
   return (
     <header className="w-full bg-transparent absolute top-0 left-0 px-4 z-50">
@@ -160,8 +275,11 @@ export default function Header() {
         {/* Desktop nav */}
         <nav className="hidden lg:flex gap-10 xl:gap-12">
           {navItems.map((item) => {
-            if (item.hasDropdown) {
+            if (item.name == "Products") {
               return <DesktopProductsDropdown key={item.name} />;
+            }
+            if (item.name == "Specialities") {
+              return <DesktopSpecialDropdown key={item.name} />;
             }
 
             return (
@@ -268,7 +386,7 @@ export default function Header() {
                         animate={{ x: 0, opacity: 1 }}
                         transition={{ delay: index * 0.1, duration: 0.3 }}
                       >
-                        {item.hasDropdown ? (
+                        {item.name == "Products" ? (
                           // Mobile Products Dropdown
                           <div>
                             <button
@@ -306,7 +424,7 @@ export default function Header() {
                                       key={product.id}
                                       href={product.href}
                                       onClick={() => setOpen(false)}
-                                      className="flex gap-3 p-4 text-white hover:bg-white/10 transition-colors border-b border-white/5 last:border-b-0"
+                                      className="flex gap-4 p-4 text-white hover:bg-white/10 transition-colors border-b border-white/5 last:border-b-0"
                                     >
                                       {/* Product Image */}
                                       <div className="relative w-16 h-16  rounded-lg flex-shrink-0 overflow-hidden">
@@ -323,6 +441,69 @@ export default function Header() {
                                       <div className="flex flex-col justify-center">
                                         <h4 className="text-sm font-semibold text-white">
                                           {product.name}
+                                        </h4>
+                                      </div>
+                                    </Link>
+                                  ))}
+                                </motion.div>
+                              )}
+                            </AnimatePresence>
+                          </div>
+                        ) : item.name == "Specialities" ? (
+                          // Mobile Products Dropdown
+                          <div>
+                            <button
+                              onClick={() =>
+                                setOpenDropdown(
+                                  openDropdown === "specialities"
+                                    ? null
+                                    : "specialities"
+                                )
+                              }
+                              className="w-full text-left text-lg py-3 px-4 rounded-lg transition-all border-b border-white/10 text-white hover:bg-white/10 flex items-center justify-between"
+                            >
+                              {item.name}
+                              <FaChevronDown
+                                className={`w-4 h-4 transition-transform ${
+                                  openDropdown === "specialities"
+                                    ? "rotate-180"
+                                    : ""
+                                }`}
+                              />
+                            </button>
+
+                            {/* Mobile Submenu */}
+                            <AnimatePresence>
+                              {openDropdown === "specialities" && (
+                                <motion.div
+                                  initial={{ opacity: 0, height: 0 }}
+                                  animate={{ opacity: 1, height: "auto" }}
+                                  exit={{ opacity: 0, height: 0 }}
+                                  transition={{ duration: 0.3 }}
+                                  className="bg-white/5 border-b border-white/10"
+                                >
+                                  {item.submenu.map((s) => (
+                                    <Link
+                                      key={s.id}
+                                      href={s.href}
+                                      onClick={() => setOpen(false)}
+                                      className="flex gap-4 p-4 text-white hover:bg-white/10 transition-colors border-b border-white/5 last:border-b-0"
+                                    >
+                                      {/* Product Image */}
+                                      <div className="relative w-10 h-10  rounded-lg flex-shrink-0 overflow-hidden">
+                                        <Image
+                                          src={s.selected}
+                                          alt={s.name}
+                                          fill
+                                          className="object-cover"
+                                          sizes="64px"
+                                        />
+                                      </div>
+
+                                      {/* Product Info */}
+                                      <div className="flex flex-col justify-center">
+                                        <h4 className="text-sm font-semibold text-white">
+                                          {s.name}
                                         </h4>
                                       </div>
                                     </Link>
